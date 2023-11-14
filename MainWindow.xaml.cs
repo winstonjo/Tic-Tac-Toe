5560dce103bc45b3326692a69471585b1e59e8c3
winstonjo <122560578+wintusy@users.noreply.github.com>
2023-11-13 17:20:33 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assignment_4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Object reference to tictactoe class.
+        /// </summary>
+        ticTacToeClass ticTac;
+
+        /// <summary>
+        /// Check if the game has started or not yet.
+        /// </summary>
+        private bool startGame;
+
+        /// <summary>
+        /// Set the player turn to be 1.
+        /// </summary>
+        private int player = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ///instantiate the object
+            ticTac = new ticTacToeClass();
+        }
+
+
+        /// <summary>
+        /// Click event button that will start the game.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void startBtn_Click(object sender, RoutedEventArgs e)
+        {
+            startGame = true;
+            Board.IsEnabled = true;
+            resetGame();
+        }
+
+        /// <summary>
+        /// Click event for all labels in the game board.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_click(object sender, RoutedEventArgs e)
+        {
+            
+            //buttons object
+            var buttons = (Button)sender;
+
+            if(startGame)
+            {
+                if(buttons.Content.ToString() == "")
+                {
+                    if(player == 1)
+                    {
+                        playerStatus.Content = "Player 1 Turn";
+                        buttons.Content = "X";
+                        LoadBoard();
+                        if(ticTac.isWinningMove())
+                        {
+                            highlightWinningMove();
+                            
+                            ticTac.Player1Wins++;
+                            player1Count.Content = "Player 1 Wins: " + ticTac.Player1Wins;
+                            startGame = false;
+
+                        }
+                        player++;   
+                    }
+                    else
+                    {
+                        playerStatus.Content = "Player 2 Turn";
+                        buttons.Content = "O";
+                        LoadBoard();
+                        if (ticTac.isWinningMove())
+                        {
+                            highlightWinningMove();
+                            
+                            ticTac.Player2Wins++;
+                            player2Count.Content = "Player 2 Wins: " + ticTac.Player2Wins;
+                            startGame = false;
+                        }
+                        player--;
+                    }
+                    if (ticTac.isTie())
+                    {
+                        ticTac.TotalTies++;
+                        tiesCount.Content = "Ties: " + ticTac.TotalTies;
+                        startGame = false;
+                    }
+                }
+            } 
+        }
+
+        /// <summary>
+        /// Load the board with values
+        /// </summary>
+        private void LoadBoard()
+        {
+            //first Col
+            ticTac.Board[0,0] = button_0_0.Content.ToString();
+            ticTac.Board[0,1] = button_0_1.Content.ToString();
+            ticTac.Board[0,2] = button_0_2.Content.ToString();
+
+            //second col
+            ticTac.Board[1,0] = button_1_0.Content.ToString();
+            ticTac.Board[1,1] = button_1_1.Content.ToString();
+            ticTac.Board[1,2] = button_1_2.Content.ToString();
+
+            //third col
+            ticTac.Board[2,0] = button_2_0.Content.ToString();
+            ticTac.Board[2,1] = button_2_1.Content.ToString();
+            ticTac.Board[2,2] = button_2_2.Content.ToString();
+        }
+        
+        /// <summary>
+        /// Reset the board to its initial State.
+        /// </summary>
+        private void resetGame()
+        {
+            //make all the buttons empty 
+            button_0_0.Content = string.Empty;
+            button_1_0.Content = string.Empty;
+            button_2_0.Content = string.Empty;
+            button_0_1.Content = string.Empty;
+            button_1_1.Content = string.Empty;
+            button_2_1.Content = string.Empty;
+            button_0_2.Content = string.Empty;
+            button_1_2.Content = string.Empty;
+            button_2_2.Content = string.Empty;
+
+            //make the background white for every button
+            button_0_0.Background = Brushes.White;
+            button_1_0.Background = Brushes.White;
+            button_2_0.Background = Brushes.White;
+            button_0_1.Background = Brushes.White;
+            button_1_1.Background = Brushes.White;
+            button_2_1.Background = Brushes.White;
+            button_0_2.Background = Brushes.White;
+            button_1_2.Background = Brushes.White;
+            button_2_2.Background = Brushes.White;
+
+            //reset status label to player 1
+            playerStatus.Content = "Player 1 Turn";
+        }
+
+        /// <summary>
+        /// Highlight the selected buttons with the winning move.
+        /// </summary>
+        private void highlightWinningMove()
+        {
+            if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.Row1)
+            {
+                button_0_0.Background = Brushes.LightGreen;
+                button_0_1.Background = Brushes.LightGreen;
+                button_0_2.Background = Brushes.LightGreen;
+            }
+
+            else if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.Row2)
+            {
+                button_1_0.Background = Brushes.LightGreen;
+                button_1_1.Background = Brushes.LightGreen;
+                button_1_2.Background = Brushes.LightGreen;
+            }
+
+            else if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.Row3)
+            {
+                button_2_0.Background = Brushes.LightGreen;
+                button_2_1.Background = Brushes.LightGreen;
+                button_2_2.Background = Brushes.LightGreen;
+            }
+
+            else if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.Col1)
+            {
+                button_0_0.Background = Brushes.LightGreen;
+                button_1_0.Background = Brushes.LightGreen;
+                button_2_0.Background = Brushes.LightGreen;
+            }
+
+            else if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.Col2)
+            {
+                button_0_1.Background = Brushes.LightGreen;
+                button_1_1.Background = Brushes.LightGreen;
+                button_2_1.Background = Brushes.LightGreen;
+            }
+
+            else if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.Col3)
+            {
+                button_0_2.Background = Brushes.LightGreen;
+                button_1_2.Background = Brushes.LightGreen;
+                button_2_2.Background = Brushes.LightGreen;
+            }
+
+            else if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.FirstDiagonal)
+            {
+                button_0_0.Background = Brushes.LightGreen;
+                button_1_1.Background = Brushes.LightGreen;
+                button_2_2.Background = Brushes.LightGreen;
+            }
+
+            else if (ticTac.GetWinningMoves == ticTacToeClass.WinningMoves.SecondDiagonal)
+            {
+                button_0_2.Background = Brushes.LightGreen;
+                button_1_1.Background = Brushes.LightGreen;
+                button_2_0.Background = Brushes.LightGreen;
+            }
+        }
+    }
+}
---
+++ ticTacToeClass.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace Assignment_4
+{
+    public class ticTacToeClass
+    {
+
+        #region Private Members With Their Properties
+
+        /// <summary>
+        /// Gameboard Representation 
+        /// </summary>
+        private string[,] board;
+
+        /// <summary>
+        /// Stats Tracker for player 1
+        /// </summary>
+        private int player1Wins;
+
+        /// <summary>
+        /// Stats Tracker for player 2
+        /// </summary>
+        private int player2Wins;
+
+        /// <summary>
+        /// Stat Tracker for all the ties
+        /// </summary>
+        private int totalTies;
+
+        /// <summary>
+        /// Winning Moves enum representation
+        /// </summary>
+        private WinningMoves eWinningMoves;
+
+        #endregion
+
+        #region Enum WinningMoves
+        /// <summary>
+        /// Enum identifying the different winning moves on the board.
+        /// </summary>
+        public enum WinningMoves
+        {
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the first row.
+            /// </summary>
+            Row1,
+
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the second row.
+            /// </summary>
+            Row2,
+
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the third row.
+            /// </summary>
+            Row3,
+
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the first column.
+            /// </summary>
+            Col1,
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the second column.
+            /// </summary>
+            Col2,
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the third column.
+            /// </summary>
+            Col3,
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the diagonal going from top left to bottom right.
+            /// </summary>
+            FirstDiagonal,
+            /// <summary>
+            /// enum that refers to the win condition when the player marks the diagonal going from top right to bottom left.
+            /// </summary>
+            SecondDiagonal
+        }
+        #endregion
+
+        #region Properties
+
+        public string[,] Board
+        {
+            get
+            {
+                return this.board;
+            }
+            set
+            {
+                this.board = value;
+            }
+        }
+
+        public int Player1Wins
+        {
+            get
+            {
+                return this.player1Wins;
+            }
+            set
+            {
+                this.player1Wins = value;
+            }
+        }
+
+        public int Player2Wins
+        {
+            get
+            {
+                return this.player2Wins;
+            }
+            set
+            {
+                this.player2Wins = value;
+            }
+        }
+
+        public int TotalTies
+        {
+            get
+            {
+                return this.totalTies;
+            }
+            set
+            {
+                this.totalTies = value;
+            }
+        }
+
+        public WinningMoves GetWinningMoves
+        {
+            get
+            {
+                return eWinningMoves; 
+            }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// initialize all corresponding private values to 0 (CONSTRUCTOR)
+        /// </summary>
+        public ticTacToeClass()
+        {
+            this.board = new string[3, 3];
+            this.player1Wins = 0;
+            this.player2Wins = 0;
+            this.totalTies = 0;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Will check for the different winning moves on the board.
+        /// </summary>
+        /// <returns> true or false </returns>
+        public bool isWinningMove()
+        {
+            return isHoriWin() || isVerWin() || isDiagWin();
+        }
+
+        /// <summary>
+        /// Checks for all the horizontal winning moves
+        /// </summary>
+        /// <returns> true or false </returns>
+        private bool isHoriWin()
+        {
+            
+            if (board[0,0] == board[0,1] && board[0,1] == board[0,2] && 
+                board[0,0] != "" && board[0,1] != "" && board[0,2] != "")
+            {
+                eWinningMoves = WinningMoves.Row1;
+                return true;
+            }
+            else if((board[1,0] == board[1,1] && board[1,1] == board[1,2] &&
+                board[1,0] != "" && board[1,1] != "" && board[1,2] != ""))
+            {
+                eWinningMoves = WinningMoves.Row2;
+                return true;
+            }
+
+            else if ((board[2,0] == board[2,1] && board[2,1] == board[2,2] &&
+                board[2,0] != "" && board[2,1] != "" && board[2,2] != ""))
+            {
+                eWinningMoves = WinningMoves.Row3;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks for all the vertical winning moves
+        /// </summary>
+        /// <returns> true or false </returns>
+        private bool isVerWin()
+        {
+            if (board[0,0] == board[1,0] && board[1,0] == board[2,0] &&
+                board[0,0] != "" && board[1,0] != "" && board[2,0] != "")
+            {
+                eWinningMoves = WinningMoves.Col1;
+                return true;
+            }
+
+            else if (board[0,1] == board[1,1] && board[1,1] == board[2,1] &&
+                board[0,1] != "" && board[1,1] != "" && board[2,1] != "")
+            {
+                eWinningMoves = WinningMoves.Col2;
+                return true;
+            }
+
+            else if (board[0,2] == board[1,2] && board[1,2] == board[2,2] &&
+                board[0,2] != "" && board[1,2] != "" && board[2,2] != "")
+            {
+                eWinningMoves = WinningMoves.Col3;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks for all the diagonal winning moves
+        /// </summary>
+        /// <returns> true or false </returns>
+        private bool isDiagWin()
+        {
+
+            if (board[0,0] == board[1,1] && board[1,1] == board[2,2] &&
+                board[0,0] != "" && board[1,1] != "" && board[2,2] != "")
+            {
+                eWinningMoves = WinningMoves.FirstDiagonal;
+                return true;
+            }
+
+            else if (board[0,2] == board[1,1] && board[1,1] == board[2,0] &&
+                board[0,2] != "" && board[1,1] != "" && board[2,0] != "")
+            {
+                eWinningMoves = WinningMoves.SecondDiagonal;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if there are no moves left on the game board.
+        /// If so, the match will end in a tie.
+        /// </summary>
+        /// <returns> true or false </returns>
+        public bool isTie()
+        {
+            if (board[0, 0] != "" && board[0, 1] != "" && board[0, 2] != "" &&
+                board[1, 0] != "" && board[1, 1] != "" && board[1, 2] != "" &&
+                board[2, 0] != "" && board[2, 1] != "" && board[2, 2] != "")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+#endregion
